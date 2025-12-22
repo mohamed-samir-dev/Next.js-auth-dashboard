8c7594b6655d771cdef1a85a300c6dd234a35cac
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-22 20:45:00 +0200

    refactor: optimize root layout with enhanced structure and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'react-loading-skeleton/dist/skeleton.css';
 import Footer from "./components/Footer";
+import ConditionalFooter from "./components/ConditionalFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
-        <Footer />
+        <ConditionalFooter />
       </body>
     </html>
   );
