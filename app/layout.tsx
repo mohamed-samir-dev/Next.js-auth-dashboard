251ce8d31027c1040421808da6b1a658547cc5ce
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-22 18:57:56 +0200

    feat: enhance main layout with improved navigation and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'react-loading-skeleton/dist/skeleton.css';
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
