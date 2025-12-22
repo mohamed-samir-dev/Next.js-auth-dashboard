61143e265aa78c389517cf4ca87d5dea8f7f5a07
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-23 00:20:06 +0200

    feat: implement dynamic sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl ='https://auth-dashboard.vercel.app'
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/auth/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/auth/register`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/category`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+  ]
+}
