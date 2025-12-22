de2dca64f84ad04fdbf13b7a00e2176b02d6af4c
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-22 17:32:36 +0200

    feat: add product grid data layer

---
+++ app/category/components/product-grid/data/index.ts
@@ -0,0 +1,51 @@
+import { Product } from '../types';
+
+export const products: Product[] = [
+  {
+    id: '1',
+    name: 'J.VER Women\'s Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello...',
+    price: 22.49,
+    originalPrice: 29.99,
+    discount: 25,
+    image: '/dress1.webp',
+    colors: ['black', 'Rose', 'navy'],
+    category: 'dress'
+  },
+  {
+    id: '2',
+    name: 'J.VER Women\'s Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello...',
+    price: 24.99,
+    originalPrice: 34.99,
+    image: '/dress2.webp',
+    colors: ['Rose', 'cream', 'light-blue'],
+    category: 'shirt'
+  },
+  {
+    id: '3',
+    name: 'J.VER Women\'s Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello...',
+    price: 22.99,
+    discount: 25,
+    image: '/dress3.webp',
+    colors: ['brown', 'tan', 'orange'],
+    category: 'shirt'
+  },
+  {
+    id: '4',
+    name: 'J.VER Women\'s Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello...',
+    price: 49.99,
+    originalPrice: 69.99,
+    image: '/dress4.webp',
+    colors: ['gray', 'black', 'navy'],
+    category: 'set'
+  },
+  {
+    id: '5',
+    name: 'J.VER Women\'s Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello...',
+    price: 49.99,
+    originalPrice: 69.99,
+    discount: 25,
+
+    image: '/dress5.webp',
+    colors: ['gray', 'black', 'navy'],
+    category: 'set'
+  }]
